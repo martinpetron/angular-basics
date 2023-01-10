2ab462822c174f4fbef3a2a1b56c47300ce42824
Martin Petron <martinpetron@outlook.com>
2023-01-10 12:00:18 -0600

    first commit

---
+++ src/app/heroes/listado/listado.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listado',
+  templateUrl: './listado.component.html'
+})
+export class ListadoComponent {
+
+  heroes: string[] = ['spiderman', 'hulk', 'ironman', 'thor'];
+  heroeBorrado: string = '';
+
+  borrarHeroe() {
+    this.heroeBorrado = this.heroes.shift() || '' ;
+    
+  }
+
+}
